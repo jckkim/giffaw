879289275e0f6f52bc4989b1843d5ee66ca5b6b9
jasonkim <jckkim@yahoo.com>
2016-08-24 08:49:26 -0700

    giffaw_project

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,30 @@
 $(document).on("ready", function(){
+  $('form').on('submit', function(event){
+    event.preventDefault();
+  $.ajax({
 
+      // What kind of request
+      method: "GET",
+
+      // The URL for the request
+      url: "http://api.giphy.com/v1/gifs/search?",
+
+      // Code to run if the request succeeds;
+      // the response is passed to the function
+      success: onSuccess,
+
+      // Code to run if the request fails; the raw request and
+      // status codes are passed to the function
+      error: onError,
+  });
+})
+  function onSuccess(json){
+    console.log(json);
+    for (var i=0; i<=json.data.length; i++){
+      $(".gif-gallery").append(json.data[i].images.original.url);
+    }
+  }
+  function onError(xhr, status, errorThrown){
+    console.log(xhr, status, errorThrown);
+  }
 });
